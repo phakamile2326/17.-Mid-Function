a9afae6f6feabbdffe4c3da5a74fafba608c9a97
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-05 02:19:44 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Dim MyPhrase As String
+        MyPhrase = InputBox("Enter Your Phrase")
+        TextBox1.Text = MyPhrase
+        TextBox1.Text = Mid(MyPhrase, 2, 6)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackColor = Color.Beige
+    End Sub
+End Class
